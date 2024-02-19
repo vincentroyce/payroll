232c01d9b9239a85a3e953f14ee785297877412f
komodu <dagliwill@gmail.com>
2024-02-19 11:23:46 +0800

    created employee and company models

--- api/payroll.go
+++ api/payroll.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-function PayrollAPIHandler(w http.ResponseWriter, r *http.Request) {
-	r.URL.Path := strings.TrimPrefix(r.URL.Path, "/api")
+func PayrollAPIHandler(w http.ResponseWriter, r *http.Request) {
+	r.URL.Path = strings.TrimPrefix(r.URL.Path, "/api")
 	r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
 }
