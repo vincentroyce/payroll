dcfbd03dd4ee51ba7c06bec632c3b6eded3b4d7d
vincentroyce <vincentroyce.coe@gmail.com>
2024-03-07 10:22:15 +0800

    optimized date

--- static/css/js/getTime.js
+++ static/css/js/getTime.js
@@ -1,61 +1,21 @@
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 let day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 let dateText = document.querySelector('.date');
-let time = new Date();
-let hour = time.getHours();
-let minute = time.getMinutes();
-let seconds = time.getSeconds();
-let date = time.getDate()
-if (hour > 12) {
-    hour -= 12;
-}
-if (hour == 0) {
-    hour = 12;
-}
-if (hour < 10) {
-    hour = `0${hour}` 
-}
-if (minute < 10) {
-    minute = `0${time.getMinutes()}` 
-}
-if (seconds < 10) {
-    seconds = `0${time.getSeconds()}`
-}
-if (date < 10) {
-    date = `0${time.getDate()}`
-}
-if (time.getHours() < 12) {
-    dateText.textContent =`${date} ${months[time.getMonth()]}, ${time.getFullYear()} ${hour}:${minute}:${seconds}AM`;
-} else {
-    dateText.textContent =`${date} ${months[time.getMonth()]}, ${time.getFullYear()} ${hour}:${minute}:${seconds}PM`;
-}
-setInterval(function() {
-    time = new Date();
-    hour = time.getHours();
-    minute = time.getMinutes();
-    seconds = time.getSeconds();
+function timeFormat() {
+    let time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let seconds = time.getSeconds();
+    let date = time.getDate()
 
-    if (hour > 12) {
-        hour -= 12;
-    }
-    if (hour == 0) {
-        hour = 12;
-    }
-    if (hour < 10) {
-        hour = `0${hour}` 
-    }
-    if (minute < 10) {
-        minute = `0${time.getMinutes()}` 
-    }
-    if (seconds < 10) {
-        seconds = `0${time.getSeconds()}`
-    }
-    if (date < 10) {
-        date = `0${time.getDate()}`
-    }
-    if (time.getHours() < 12) {
-        dateText.textContent =`${date} ${months[time.getMonth()]}, ${time.getFullYear()} ${hour}:${minute}:${seconds}AM`;
-    } else {
-        dateText.textContent =`${date} ${months[time.getMonth()]}, ${time.getFullYear()} ${hour}:${minute}:${seconds}PM`;
-    }
-},1000)
+    let meridiem = (hour >= 12) ? "PM":"AM";
+    hour = (hour == 0 || hour == 12) ? 12 : hour % 12;
+    hour = (hour < 10) ? `0${hour}`: `${hour}`;
+    minute = (minute < 10) ? `0${minute}`: `${minute}`;
+    seconds = (seconds < 10) ? `0${seconds}`: `${seconds}`;
+    date  = (date < 10) ? `0${date}`: `${date}`;
+
+    dateText.textContent = `${date} ${months[time.getMonth()]}, ${time.getFullYear()} ${hour}:${minute}:${seconds}${meridiem}`;
+} 
+timeFormat();
+setInterval(timeFormat, 1000);
